cce32add8126e0063025350d49dd2dff119dedfe
Clara <39087696+claramanterola@users.noreply.github.com>
2019-09-16 20:57:14 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+  if (isset($_POST)) {
+    if (isset($_POST["user-name"])) {
+      $errorUserName = true;
+      $classUserName = "El usuario o contraseña ingresado es incorrecto";
+    }
+  }
+
+
+
+ ?>
