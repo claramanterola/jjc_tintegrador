352681188cdcd7d198f4cd101ec26e104514876f
Juan S <juan.stillo1@gmail.com>
2019-09-17 23:55:32 -0300

    arrays

---
+++ Arrayproductos.php
@@ -0,0 +1,26 @@
+<?php
+$raquetas = [
+  "marcas" => [ "Babolat","Tecnifibre","Head","Wilson","Yonex"],
+    "modelos" => ["niños", "adultos"]
+];
+
+$pelotas = [
+"marcas" => [ "Babolat","Head","Penn","Wilson"],
+"cantidad" => ["sueltas","tubox3","tubox4"]
+];
+
+$cuerdas = [
+    "marcas" => ["Luxilon","Kirschbaum","Solinco"],
+    "tipo" => ["monofilamento","multifilamento"]
+    ];
+
+$bolsos = [
+     "marcas" => [ "Babolat","Tecnifibre","Head","Wilson","Yonex"],
+     "capacidad" => ["simple","doble","triple"]
+     ];
+$accesorios = ["antivibradores", "cubregrip","grip","muñequeras"
+    ];
+
+var_dump($bolsos);
+
+ ?>
