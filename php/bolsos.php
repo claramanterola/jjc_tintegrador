ce6ce4f08f4cc6454714889905510a9ac0adf534
bancorajulieta <bancorajulieta@hotmail.com>
2019-09-30 21:49:04 -0300

    pase de html a php

---
+++ php/bolsos.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="shortcut icon" href= "img/img1.png" class="next-head">
+    <link rel="stylesheet" href="styles/styles.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Raquetas</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <?php
+      include("../header.html");
+      ?>
+    </div>
+
+    <main>
+      <section id="use-cases" class="py-4 my-4">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-12 col-md-6 col-lg-4 text-center my-4">
+              <div class="card" style="width: 18rem">
+                <img src="https://www.tennisplaza.com/prodimages/27792-DEFAULT-s.jpg"class="card-img-top" alt="Babolatraquect">
+                <div class="card-body">
+                  <h5 class="card-title">Babolat</h5>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-12">
+                        <a href="raquetas-babolat.html" class="btn btn-primary">Ver Todos</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 text-center my-4">
+              <div class="card" style="width: 18rem">
+                <img src="https://www.tennisplaza.com/prodimages/19553-DEFAULT-s.jpg"class="card-img-top" alt="Tecnifibreraquect">
+                <div class="card-body">
+                  <h5 class="card-title">Tecnifibre</h5>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-12">
+                        <a href="#" class="btn btn-primary">Ver Todos</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 text-center my-4">
+              <div class="card" style="width: 18rem">
+                <img src="https://www.tennisplaza.com/prodimages/24415-DEFAULT-s.jpg"class="card-img-top" alt="Headracquect">
+                <div class="card-body">
+                  <h5 class="card-title">Head</h5>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-12">
+                        <a href="#" class="btn btn-primary">Ver Todos</a>
+                      </div>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12 col-md-6 col-lg-4 text-center my-4">
+                <div class="card" style="width: 18rem">
+                  <img src="https://www.tennisplaza.com/prodimages/24055-DEFAULT-s.jpg"class="card-img-top" alt="Wilsonracquet">
+                    <div class="card-body">
+                      <h5 class="card-title">Wilson</h5>
+                      <div class="container">
+                        <div class="row">
+                          <div class="col-12">
+                            <a href="#" class="btn btn-primary">Ver Todos</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-4 text-center my-4">
+                  <div class="card" style="width: 18rem">
+                    <div class="card-body">
+                      <h5 class="card-title">Yonex</h5>
+                      <div class="container">
+                        <div class="row">
+                          <div class="col-12">
+                            <a href="#" class="btn btn-primary">Ver Todos</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-4 text-center my-4">
+                  <div class="card" style="width: 18rem">
+                    <img src="https://www.tennisplaza.com/prodimages/22851-DEFAULT-s.jpg"class="card-img-top" alt="Cuerdas">
+                    <div class="card-body">
+                      <h5 class="card-title">Promociones</h5>
+                      <div class="container">
+                        <div class="row">
+                          <div class="col-12">
+                            <a href="#" class="btn btn-primary">Ver Todas</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
+      </section>
+    </main>
+
+    <footer>
+      <?php
+      include("../footer.html");
+      ?>
+    </footer>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/addc1ca592.js"></script>
+  </body>
+</html>
---
+++ php/contact.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href= "../img/img1.png" class="next-head">
+    <link rel="stylesheet" href="../styles/styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Contacto</title>
+  </head>
+
+  <body id="principal">
+
+    <div class="container-fluid">
+      <?php
+      include("../header.html");
+      ?>
+    </div>
+
+    <main>
+      <container class="main-content">
+        <div id="row">
+
+          <div class="col-lg6 col-12">
+            <div class="title-des">
+              <h1 id="main-title">Contactanos</h1>
+            </div>
+
+            <div class="main-form">
+              <form action="" method="POST" enctype="multipart/form-data">
+
+                <div class="form-group">
+                  <label for="name">Nombre</label>
+                  <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Juan Martín">
+                </div>
+
+                <div class="form-group">
+                  <label for="lastname">Email</label>
+                  <input type="email" class="form-control" id="lastname" aria-describedby="lastnameHelp" placeholder="jmdelpotro@gmail.com">
+                </div>
+
+                <div class="form-group">
+                  <label for="telephone">Teléfono (opcional)</label>
+                  <input type="telephone" class="form-control" id="telephone" aria-describedby="telephoneHelp">
+                  <small id="telephoneHelp" class="form-text text-muted">Sin 0 y sin 15 para celulares</small>
+                </div>
+
+                <div class="form-group">
+                  <label for="exampleFormControlTextarea1">Mensaje (opcional)</label>
+                  <textarea class="form-control" id="textMessage" rows="3" placeholder="Dejanos aquí tu mensaje o consulta"></textarea>
+                </div>
+
+                <div id="create">
+                  <button type="button" class="btn btn-dark create">Enviar</button>
+                </div>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </container>
+    </main>
+
+  <footer>
+    <?php
+    include("../footer.html");
+    ?>
+  </footer>
+
+  </body>
+</html>
