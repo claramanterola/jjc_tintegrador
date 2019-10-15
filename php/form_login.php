0cb78bac3cc28529951795642255e987ec34e427
Juan S <juan.stillo1@gmail.com>
2019-10-14 23:44:41 -0300

    domingo 14-

--- php/form_login.php
+++ php/form_login.php
@@ -1,255 +1,91 @@
 <?php
-<<<<<<< HEAD
-  if($_POST){
-    echo "se hizo un Post";exit;
-=======
-  require_once('controladores/funciones.php');
+  require_once('funciones.php');
   require_once('helpers.php');
-  
   if($_POST){
     $errores = validarLogin($_POST);
     if(count($errores)==0){
       $usuario = buscarPorEmail($_POST['email']);
-      
       if($usuario==null){
         $errores['email']="Usuario no encontrado...";
-      } else {
-
+      }else{
         //Desde aquí incio mi revisión a ver que ocurre con los contenidos de las variables y tratar de ver que ocurre
         //Aquí ví que el dato viene bien, es decir el password hasheado
         //dd($usuario['password']);
         //Ahora veo que trae esta variable  y noto que trae el dato correctamente
         //dd($_POST['password']);
         //Aquí estaba el error a la función password_verify, se le debe psar primero el dato no hasheado y luego el hasheado, ese fue mi error, lo habia pasado al contrario
-
-        if(password_verify($_POST['password'],$usuario['password'])===false) {
+        if(password_verify($_POST['password'],$usuario['password'])===false){
           $errores['password']="Datos inválidos...";
-        } else {
+        }else{
           seteoUsuario($usuario,$_POST);
-
           if(validarUsuario()){
             header('location:perfil.php');
             exit;
-
-      		} else {
-      			header('location:login.php');
-      			exit;
-            	}
+          }else{
+            header('location:login.php');
+            exit;
           }
-	     }
+        }
+      }
     }
->>>>>>> 4aa5df1e484902fc9b2200c5107edd78ccc44942
   }
+
 ?>
 
-
-
-
-<!DOCTYPE html>
-<html lang="es" dir="ltr">
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<<<<<<< HEAD
-
-=======
->>>>>>> 944daecd3aa0970f96ed96d8cf9cb979187dbe7d
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  
-  <link rel="stylesheet" href="../styles/stylesClara.css">
-  <link rel="shortcut icon" href= "../img/__logo/flavicon.ico" class="next-head">
-  <!-- FONTS -->
-  <link href="https://fonts.googleapis.com/css?family=Staatliches&display=swap" rel="stylesheet">
-  <title>JCJ | Ingresá a tu cuenta</title>
-</head>
-
-<<<<<<< HEAD
-<body id="formsbody">
-	
-	<header>
-    <?php
-    include_once "../header.php";
-    ?>
-  </header>
-=======
-<<<<<<< HEAD
-    <title>JCJ | Iniciar sesion</title>
-
-=======
-    <title>Iniciá sesión</title>
+<html>
   <head>
+    <title>Formulario de Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/index.css">
+
     <meta charset="utf-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/stylesClara.css">
-    <link rel="shortcut icon" href= "img/img1.png" class="next-head">
-    <!-- FONTS -->
-    <link href="https://fonts.googleapis.com/css?family=Staatliches&display=swap" rel="stylesheet">
-
-    <title>JCJ | Iniciar sesion</title>
->>>>>>> 0ef5f45791cddbe3541cf6cc257997e8831b30f6
   </head>
-  <body id="formsbody">
+  <body>
+    <div class="container-fluid containerLogin">
+      <?php include_once('../header.php');?>
+      <section class="formularioLogin">
+        <div id="formContainer" class="row align-items-center">
+          <div class="col-8 offset-2  ">
+            <h1>Iniciar Sesión</h1>
+            <?php if(isset($errores)):?>
+              <ul class="alert alert-danger">
+                <?php foreach ($errores as $value) :?>
+                    <li><?=$value;?></li>
+                <?php endforeach;?>
+              </ul>
+            <?php endif;?>
+            <form action="form_login.php" id="formulario"  class="form" name="formLogin"   method="POST" enctype="multipart/form-data" >
 
-    <header>
-      <?php
-      include_once "../header.php";
-      ?>
-    </header>
->>>>>>> 944daecd3aa0970f96ed96d8cf9cb979187dbe7d
-
-    <container class="main-content">
-      <div id="row">
-        <div class="col-lg6 col-12">
-
-        <!--TITULO, DESCRIPCION Y FORMULARIO-->
-        <div class="title-des">
-          <h1 id="main-title_form">Ingresá a tu cuenta</h1>
-          <h2 id="description-title">Completa tus datos a continuación para poder ingresar a tu cuenta:<br></h2>
-        </div>
-
-        <!--FORM-->
-        <!--Usuario-->
-        <div class="main-form">
-<<<<<<< HEAD
-          <form action="" method="post" enctype="multipart/form-data">
-            <div class="form-group">
-              <label for="email">Usuario</label>
-              <input type="email" class="form-control" name="name" id="mail" aria-describedby="emailHelp" placeholder="jmdelpotro@mail.com">
-=======
-         <form action="loginValidation.php" method="POST">
-            <div class="form-group">
-              <label for="email">Usuario/email</label>
-              <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="jmdelpotro@mail.com" required>
->>>>>>> 4aa5df1e484902fc9b2200c5107edd78ccc44942
-            </div>
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 0ef5f45791cddbe3541cf6cc257997e8831b30f6
->>>>>>> 944daecd3aa0970f96ed96d8cf9cb979187dbe7d
-        <!--Password-->
-          <div class="pass-group">
-            <label for="password">Password</label>
-<<<<<<< HEAD
-            <input type="password" class="form-control" name="password" id="password">
-=======
-            <input type="password" class="form-control" id="password" name="password" required> 
-            <small id="passHelp" class="form-text text-muted">Debe contener al menos 6 caracteres</small>
-          
-          <!--
-          BOTON ANTERIOR
->>>>>>> 4aa5df1e484902fc9b2200c5107edd78ccc44942
-            <label class="form-check-label" for="text"> <a href="#">Olvidé mi contraseña</a></label>
-          -->
-
-            <button type="button" class="btn btn-link" data-toggle="modal" data-target="#forgotModal">Olvidé mi contraseña
-            </button>
-          </div>
-
-          <div class="modal fade" id="forgotModal" tabindex="-1" role="dialog" aria-labelledby="forgotPasswordModal" aria-hidden="true">
-
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="forgotPasswordModal">Olvidé mi contraseña</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
+              <div class="form-group">
+                <label for="email">Correo electrónico</label>
+                <input required name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Ingrese su correo" value= "<?=isset($errores['email'])? "":old('email') ;?>">
               </div>
-              <div class="modal-body">
-                <label for="email">Ingresá tu email:</label>
-              <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="jmdelpotro@mail.com" required>
+
+              <div class="form-group">
+                <label for="password">Contraseña</label>
+                <input required name="password" type="password" value= "" class="form-control" id="password" placeholder="Contraseña">
+
+                <small class="form-text text-muted">Al menos 6 caracteres, debe contenter sólo números</small>
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-link" data-dismiss="modal">Cancelar</button>
-                <button type="button" id="main-button-black"> <a href="../indexClara.html">Recuperar</a></button>
+
+              <div class="form-group">
+                <input  class="text-left" name="recordarme" type="checkbox" value= "recordarme" class="form-control " id="recordarme" >
+                <label for="recordarme">Recordarme</label>
               </div>
-            </div>
-          </div>
-        </div>
 
-          <!--Recordar usuario-->
-          <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="remember-user">
-            <label class="form-check-label" for="remember-user" name="remember-user">Recordar mi usuario</label>
-          </div>
-        </div>
-<<<<<<< HEAD
-        <div id="create">
-          <button type="button" id="main-button-black"> <a href="form_login.php">Ingresar</a></button>
-
-
-<<<<<<< HEAD
-	        <div id="create">
-	        	<button type="submit" id="main-button-black" name="submit">Ingresar</button>
-	        </div>
-       	</form>
-=======
-            <!--Password-->
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" class="form-control" id="password">
-              <label class="form-check-label" for="text"> <a href="#">Olvidé mi contraseña</a></label>
-            </div>
-
-            <!--Recordar usuario-->
-            <div class="form-group form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1">
-              <label class="form-check-label" for="exampleCheck1">Recordar mi usuario</label>
-            </div>
-
-=======
-
->>>>>>> 0ef5f45791cddbe3541cf6cc257997e8831b30f6
-            <div id="create">
               <button type="submit" class="btn btn-primary">Ingresar</button>
-            </div>
-          </form>
-<<<<<<< HEAD
+              <a href="register.php" class="btn btn-link">Aun no poseo una cuenta</a>
+              <a href="olvidePassword.php" class="btn btn-link">¿Olvidaste tu contraseña?</a>
+            </form>
         </div>
-=======
->>>>>>> 0ef5f45791cddbe3541cf6cc257997e8831b30f6
->>>>>>> 944daecd3aa0970f96ed96d8cf9cb979187dbe7d
+      </section>
+      <?php include_once('../footer.php');?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
-        <div class="new-user">
-          <p id="current-id">¿Aún no tenés cuenta?</p>
-          <button type="button" id="main-button-black" name="new-user" background-color="#D4DC00"><a href="form_registro.php">Creá tu usuario</a></button>
-        </div>
-      </div>
-    </container>
-    <!--A C Á   T E R M I N A   E L   F O R M U L A R I O-->
-
-<<<<<<< HEAD
-
-
-    <!--FOOTER -->
-
-    <footer>
-      <?php
-      include_once "../footer.php";
-      ?>
-    </footer>
-
-=======
->>>>>>> 4aa5df1e484902fc9b2200c5107edd78ccc44942
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-      <script src="https://kit.fontawesome.com/addc1ca592.js"></script>
-    </body>
-
-  <!--FOOTER 
-  <footer>
-    <?php
-    include_once "../footer.php";
-    ?>
-  </footer>
-  -->
+  </body>
 </html>
--- php/form_registro.php
+++ php/form_registro.php
@@ -1,22 +1,41 @@
 <?php
-require("funciones.php");
-require("encrip.php");
+require_once("funciones.php");
+require_once("encrip.php");
+
+if($_POST){
+   $errores = validar($_POST,$_FILES);
+   if(count($errores)==0){
+    $user = buscarPorEmail($_POST["email"]);
+    if($user !== null){
+      $errores["email"]="Usuario ya registrado";
+    }else{
+      $avatar = armarAvatar($_FILES);
+      $registro = armarRegistro($_POST,$avatar);
+      guardarRegistro($registro);
+
+      //De no existir errores en la información tipeada por el usuario entonces lo redirecciono a donde yo desee.
 
 
-  $users = obtenerUsuarios();
+        }
+      }
+    }
+
+
+
+/*$users = obtenerUsuarios();
 
   if($_POST){
     $user = crearUsuario();
     $id = count($users) + 1;
     $user["id"] = $id;
-    $user["profilePicture"] = "pictures/$id.jpg";
+    $user["uploadAvatar"] = "pictures/$id.jpg";
     $users[] = $user;
     $json = json_encode($users);
     file_put_contents("users.json", $json);
-    $file = $_FILES["uploadAvatar"];
+    $avatar = $_FILES["avatar"];
     move_uploaded_file($file["tmp_name"], "pictures/$id.jpg");
   }
-
+  */
 ?>
 
 <!DOCTYPE html>
@@ -25,20 +44,10 @@ require("encrip.php");
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
-  <link rel="stylesheet" href="../styles/stylesClara.css">
-  <link rel="shortcut icon" href= "../img/__logo/flavicon.ico" class="next-head">
-
-=======
->>>>>>> 4aa5df1e484902fc9b2200c5107edd78ccc44942
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="../styles/styles.css">
   <link rel="shortcut icon" href= "../img/img1.png" class="next-head">
->>>>>>> 944daecd3aa0970f96ed96d8cf9cb979187dbe7d
   <!-- FONTS -->
   <link href="https://fonts.googleapis.com/css?family=Staatliches&display=swap" rel="stylesheet">
 
@@ -50,14 +59,6 @@ require("encrip.php");
 
     </header>
 
-    <header>
-      <?php
-      include_once "../header.php";
-      ?>
-    </header>
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <div class="main-content">
       <div class="container">
@@ -70,6 +71,17 @@ require("encrip.php");
             <h2 id="description-title">Completa tus datos a continuación para poder crear tu cuenta:</h2>
           </div>
 
+
+          <?php if(isset($errores)):?>
+             <ul class="alert alert-danger">
+               <?php foreach ($errores as $value) :?>
+                   <li><?=$value;?></li>
+               <?php endforeach;?>
+             </ul>
+           <?php endif;?>
+
+
+
     <!--BUTTONS-->
           <div id="social-button">
             <button type="button" id="main-button-google" name="login-google"><a href="form_registro.php">Ingresar con Google</a></button>
@@ -79,22 +91,22 @@ require("encrip.php");
 
     <!--FORM-->
           <div class="main-form">
-            <form action="" method="post" enctype="multipart/form-data">
+            <form action="form_registro.php" method="post" enctype="multipart/form-data">
               <input type='hidden' name='submitted' id='submitted' value='1'/>
 
               <div class="form-group">
                 <label for="name">Nombre</label>
-                <input required type="text" class="form-control" name="name" id="name" aria-describedby="nameHelp" placeholder="Nombre">
+                <input required type="text" class="form-control" name="name" id="name" aria-describedby="nameHelp" placeholder="Nombre"  >
               </div>
 
               <div class="form-group">
                 <label for="lastname">Apellido</label>
-                <input required type="lastname" class="form-control" name="lastname" id="lastname" aria-describedby="lastnameHelp" placeholder="Apellido">
+                <input required type="lastname" class="form-control" name="lastname" id="lastname" aria-describedby="lastnameHelp" placeholder="Apellido" >
               </div>
 
               <div class="form-group">
                 <label for="email">Email</label>
-                <input required type="email" class="form-control" name="email" id="mail" aria-describedby="emailHelp" placeholder="tumail@mail.com">
+                <input required type="email" class="form-control" name="email" id="mail" aria-describedby="emailHelp" placeholder="tumail@mail.com" >
               </div>
 
               <div class="form-group">
@@ -104,7 +116,7 @@ require("encrip.php");
 
               <div class="form-group">
                 <label for="telephone">Teléfono</label>
-                <input required type="telephone" class="form-control" name="telephone" id="telephone" aria-describedby="telephoneHelp" placeholder="">
+                <input required type="telephone" class="form-control" name="telephone" id="telephone" aria-describedby="telephoneHelp" placeholder="" >
                 <small id="telephoneHelp" class="form-text text-muted">Sin 0 y sin 15 para celulares</small>
               </div>
 
@@ -112,17 +124,36 @@ require("encrip.php");
                 <label for="inputCity">Ciudad</label>
                 <select name="inputCity" id="inputCity" class="form-control">
                   <option selected>Seleccioná una opción</option>
-                  <option>Capital Federal</option>
-                  <option>Córdoba</option>
-                  <option>GBA Gran Buenos Aires</option>
-                  <option>Santa Fe</option>
+                  <option value="Buenos Aires">Bs. As.</option>
+                  <option value="Catamarca">Catamarca</option>
+                  <option value="Chaco">Chaco</option>
+                  <option value="Chubut">Chubut</option>
+                  <option value="Cordoba">Cordoba</option>
+                  <option value="Corrientes">Corrientes</option>
+                  <option value="Entre Rios">Entre Rios</option>
+                  <option value="Formosa">Formosa</option>
+                  <option value="Jujuy">Jujuy</option>
+                  <option value="La Pampa">La Pampa</option>
+                  <option value="La Rioja">La Rioja</option>
+                  <option value="Mendoza">Mendoza</option>
+                  <option value="Misiones">Misiones</option>
+                  <option value="Neuquen">Neuquen</option>
+                  <option value="Rio Negro">Rio Negro</option>
+                  <option value="Salta">Salta</option>
+                  <option value="San Juan">San Juan</option>
+                  <option value="San Luis">San Luis</option>
+                  <option value="Santa Cruz">Santa Cruz</option>
+                  <option value="Santa Fe">Santa Fe</option>
+                  <option value="Sgo. del Estero">Sgo. del Estero</option>
+                  <option value="Tierra del Fuego">Tierra del Fuego</option>
+                 <option value="Tucuman">Tucuman</option>
                 </select>
               </div>
 
               <div class="form-group">
-                <label for="uploadAvatar">Foto de perfil</label>
-                <input required type="file" class="form-control-file" name="uploadAvatar" id="uploadAvatar">
-                <small id="uploadAvatar" class="form-text text-muted">No debe pesar más de 20mb, en formato .jpg, .jpeg, .png <br></small>
+                <label for="avatar">Foto de perfil</label>
+                <input required type="file" class="form-control-file" name="avatar" id="avatar" >
+                <small id="uploadAvatar" class="form-text text-muted">No debe pesar más de 20mb, en formato .jpg o .png <br></small>
               </div>
 
               <div id="create">
@@ -140,22 +171,10 @@ require("encrip.php");
         </div> <!-- .row -->
       </div> <!-- .container -->
     </div> <!-- .main-content -->
-    <footer>
 
-<<<<<<< HEAD
-    </footer>
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="https://kit.fontawesome.com/addc1ca592.js"></script>
-=======
   <!--FOOTER -->
   <footer>
-    <?php
-    include_once "../footer.php";
-    ?>
+
   </footer>
 
 
@@ -165,7 +184,5 @@ require("encrip.php");
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/addc1ca592.js"></script>
-
->>>>>>> 4aa5df1e484902fc9b2200c5107edd78ccc44942
   </body>
 </html>
